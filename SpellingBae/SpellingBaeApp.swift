aad3cf806309f67271926ba1554e7efb0beea9f9
rahdeva <wirandanaputra@gmail.com>
2026-07-18 17:04:00 +0700

    Initial Commit

---
+++ SpellingBae/SpellingBaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBaeApp.swift
+//  SpellingBae
+//
+//  Created by rahdeva on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
